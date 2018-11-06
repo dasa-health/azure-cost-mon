02c09951146b1caf17fa16bac3ffc44fcc55a93a
Vitor Schiavo <vitor.schiavo@dasa.com.br>
2018-11-06 17:46:23 -0200

    [DevOps] - azure cost

--- azure_costs_exporter/main.py
+++ azure_costs_exporter/main.py
@@ -12,4 +12,4 @@ def create_app(configuration_path):
 
 if __name__ == "__main__":
     app = create_app(path.join(getcwd(), "application.cfg"))
-    app.run(debug=True, host='0.0.0.0')
+    app.run(host='0.0.0.0',port=8000)
